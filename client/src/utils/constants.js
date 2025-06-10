13435e9effe2cef52a302dd576bda0570c6e0bac
Omkarsawant10 <122078907+Omkarsawant10@users.noreply.github.com>
2025-06-10 17:05:03 +0530

    url changed

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT="http://localhost:5000/api/v1/user";
-export const PRODUCT_API_END_POINT="http://localhost:5000/api/v1/product";
+export const USER_API_END_POINT="https://inventory-dashboard-backend-dobt.onrender.com/api/v1/user";
+export const PRODUCT_API_END_POINT="https://inventory-dashboard-backend-dobt.onrender.com/api/v1/product";
